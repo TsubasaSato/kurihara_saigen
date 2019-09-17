8e88b469f212addd745ed8f3a395f4a2a0547733
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2019-09-17 12:12:37 +0900

    Update kuriharasan.py

--- kuriharasan.py
+++ kuriharasan.py
@@ -121,7 +121,7 @@ class Kurihara15(app_manager.RyuApp):
         datapath.send_msg(self.create_flow_mod(datapath, 0,3, 
                                                parser.OFPMatch(eth_type=0x0800, 
                                      ip_proto=6,tcp_flags=self.TCP_RST),inst)) 
-        # exchange IP,MAC,PORT  reg valiable must have a trailing number??
+        # exchange IP,MAC,PORT
         actions1 =[parser.OFPActionCopyField(n_bits=32,oxm_ids=[parser.OFPOxmId('ipv4_src'), parser.OFPOxmId('reg1')]),
                    parser.OFPActionCopyField(n_bits=32,oxm_ids=[parser.OFPOxmId('ipv4_dst'), parser.OFPOxmId('ipv4_src')]),
                    parser.OFPActionCopyField(n_bits=32,oxm_ids=[parser.OFPOxmId('reg1'), parser.OFPOxmId('ipv4_dst')]),
@@ -132,7 +132,7 @@ class Kurihara15(app_manager.RyuApp):
                    parser.OFPActionCopyField(n_bits=16,oxm_ids=[parser.OFPOxmId('tcp_dst'), parser.OFPOxmId('tcp_src')]),
                    parser.OFPActionCopyField(n_bits=16,oxm_ids=[parser.OFPOxmId('reg2'), parser.OFPOxmId('tcp_dst')])
                   ]
-                  #,parser.OFPActionSetField(tcp_flags=self.TCP_SYN) 48 16
+                  #parser.OFPActionSetField(tcp_flags=self.TCP_SYN)
                   
         actions1 += flow11
         inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
