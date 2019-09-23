aec0ce21b9b4114244adaf20871419b75e74a49b
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2019-09-23 17:48:26 +0900

    Update kuriharasan.py

--- kuriharasan.py
+++ kuriharasan.py
@@ -130,8 +130,8 @@ class Kurihara15(app_manager.RyuApp):
                    parser.OFPActionCopyField(n_bits=48,oxm_ids=[parser.OFPOxmId('xxreg3'), parser.OFPOxmId('eth_dst')]),
                    parser.OFPActionCopyField(n_bits=16,oxm_ids=[parser.OFPOxmId('tcp_src'), parser.OFPOxmId('reg2')]),
                    parser.OFPActionCopyField(n_bits=16,oxm_ids=[parser.OFPOxmId('tcp_dst'), parser.OFPOxmId('tcp_src')]),
-                   parser.OFPActionCopyField(n_bits=16,oxm_ids=[parser.OFPOxmId('reg2'), parser.OFPOxmId('tcp_dst')]),
-                   parser.OFPActionSetField(tcp_flags=self.TCP_SYN)
+                   parser.OFPActionCopyField(n_bits=16,oxm_ids=[parser.OFPOxmId('reg2'), parser.OFPOxmId('tcp_dst')])
+                   #parser.OFPActionSetField(tcp_flags=self.TCP_SYN)
                   ]
                   
         actions1 += flow11
